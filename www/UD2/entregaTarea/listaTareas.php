a19c3dd8ff0468be6f461b97af93d0b4720529bf
Martín Domínguez Díaz <martin-dominguez12@outlook.es>
2024-11-04 20:35:15 +0100

    Actualización 04/11 20:35

--- www/UD2/entregaTarea/listaTareas.php
+++ www/UD2/entregaTarea/listaTareas.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UD2. Tarea</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <?php include 'header.php'; ?> 
+    <div class="container-fluid">
+        <div class="row">
+            <?php include 'menu.php'; ?>
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+
+
 <div class="table">
     <table class="table table-striped table-hover">
         <thead class="thead">
@@ -12,4 +29,11 @@
                 <td></td>
                 ...
             </tr>
-        ...
+
+
+            </main>
+        </div>
+    </div>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
