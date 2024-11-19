0945c59e5d5d29bce69a5cb507f1078ac637fd52
Martín Domínguez Díaz <martin-dominguez12@outlook.es>
2024-11-19 20:40:32 +0100

    Actualización 19/11/24 20:41 restauración de docker-compose.yml original

--- www/UD2/entregaTarea/nueva.php
+++ www/UD2/entregaTarea/nueva.php
@@ -2,19 +2,21 @@
 
 include 'utils.php';
 
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre_tarea = $_POST["nombre_tarea"];
     $estado = $_POST["estado"];
     $contenido = $_POST["contenido"];
 
-    
     $guardar_datos = guardar_datos($nombre_tarea, $estado, $contenido);
 
-    if ($guardar_datos == true) {
-        echo "Datos guardados correctamente";
-    } else {
-        echo "Error, verifica los datos y vuelve a intentarlo";
-    }
+if ($guardar_datos == true) {
+
+    echo "Datos guardados correctamente";
+} else {
+    echo "Error, verifica los datos y vuelve a intentarlo";
 }
+}
+
 
 ?>
