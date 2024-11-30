483b02af6db5f682a01b5447d8b4b40629d22287
misterstin <martin-dominguez12@outlook.es>
2024-11-30 13:27:43 +0100

    update 30/11/2024 13:27

--- www/UD03/entregaTarea/init.php
+++ www/UD03/entregaTarea/init.php
@@ -9,11 +9,33 @@ $password = "test";
 
 try{
 
-$conPDO = new PDO("mysql:host=$servername;dbname=tareas", $username, $password);
+$conPDO = new PDO("mysql:host=$servername", $username, $password);
 
 //forzar excepciones
 $conPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //Esto siempre hay que ponerlo igual
 echo "Conexión correcta";
+/*Comprobamos si la base de datos existe buscandola mediante sentencia sql si devuelve algún
+resultado, nos conectamos a ella sobreescribiendo conPDO, sino la creamos*/
+$dbName = "tareas";
+$stmt = $conPDO->query("SHOW DATABASES LIKE '$dbName'");
+
+if($stmt->rowCount() > 0){
+    $conPDO = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
+    $conPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}else{
+
+    $sql = "CREATE DATABASE tareas";
+    $conPDO->exec($sql);
+    echo "Base de datos creada correctamente";
+
+    //Nos conectamos a la base de datos recien creada
+
+    $conPDO = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
+    $conPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión correcta a la base de datos recién creada.";
+
+}
+
 
 
 }catch(PDOExecption $e){
@@ -23,6 +45,6 @@ echo "Conexión correcta";
 }
 
 
-
+$conPDO = null //Cerrar base de datos siempre al terminar
 
 ?>
